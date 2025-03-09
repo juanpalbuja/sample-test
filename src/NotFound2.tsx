ac7fa43ea283a78e2917c8b0888b119172b7b0e0
Juan Pablo Albuja <jalbuja@mergeworld.com>
2025-03-09 14:21:35 -0500

    Change a bit the maiteinance

--- src/NotFound2.tsx
+++ src/NotFound2.tsx
@@ -1,13 +1,8 @@
-import Head from 'next/head';
-
 /**
  * Rendered in case if we have 404 error
  */
 const NotFound2 = (): JSX.Element => (
   <>
-    <Head>
-      <title>500: ERROR</title>
-    </Head>
     <div style={{ padding: 10 }}>
       <h1>Crashed</h1>
       <p>Crashed</p>
